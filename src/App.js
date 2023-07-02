0387376561b4ec166c38ecc0d682928efb4baa89
Colten Wasden <colt45wasden@gmail.com>
2023-07-02 18:54:46 -0400

    part 1 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// const Hello = (props) => {
+//   console.log(props)
+//   return (
+//    <div>
+//     <p>
+//       Hello {props.name}, you are {props.age} years old
+//       </p>
+//    </div>
+//   )
+// }
+// const Footer = () => {
+//   return (
+//     <div>
+//       greeting app created by <a href='https://github.com/mluukai'>mluukkai</a>
+//     </div>
+//   )
+// }
+const App = () => {
+  const friends = [
+    {name: 'Peter', age: 4 },
+    {name: 'Janet', age: 82 },
+  ]
 
-function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <p>this is my friend {friends[0].name} he is {friends[0].age} years old</p>
+     <p>{friends[1].name} {friends[1].age}</p>
+
     </div>
-  );
+  )
 }
 
 export default App;
